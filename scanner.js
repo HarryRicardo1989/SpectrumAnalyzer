4bc8d6511a02498b86ca1074d8471c20ce563c69
Ricardo Paschoali <ricardo.paschoali@gmail.com>
2021-10-01 15:03:34 +0100

    ajust

--- scanner.js
+++ scanner.js
@@ -5,19 +5,20 @@ class Scanner {
         this.freq_init = freq_init
         this.freq_end = freq_end
         this.freq_step = freq_step
-    }
+    };
 
     scan() {
 
-        console.log(this.freq_init)
-        console.log(this.freq_end)
-        console.log(this.freq_step)
+        console.log(this.freq_init);
+        console.log(this.freq_end);
+        console.log(this.freq_step);
         execSync(`echo {\\\"scan\\\":[\`rtl_power -f ${this.freq_init}e6:${this.freq_end}e6:${this.freq_step}e3 -g 30 -i 1 -1 -d 0 | awk '{print"["substr($0, index($0,$3))"],"}' | xargs | sed 's/.$//' \`]} > scan.json`);
         
         return ;
-            
-    
 
+    };
+    readFileScan() {
+        
     }
 
 }
